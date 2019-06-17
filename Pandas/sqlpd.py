ef7cf207618c385e1d4eab906c8981f1651e0297
Demiurg-in-real <bezginandrew@gmail.com>
2019-06-17 18:15:44 -0400

    Sm chages

--- Pandas/sqlpd.py
+++ Pandas/sqlpd.py
@@ -10,3 +10,11 @@ df=pd.read_sql('SELECT * FROM table_name', con=db_connection)
 #db.cursor.execute('SELECT * FROM table_name')
 #table_rows=db_cursor.fetchall()
 #df=pd.DataFrame(table_rows)
+
+import MySQLdb
+mysql_cn= MySQLdb.connect(host='myhost',
+                port=3306,user='myusername', passwd='mypassword',
+                db='information_schema')
+df_mysql = pd.read_sql('select * from VIEWS;', con=mysql_cn)
+print 'loaded dataframe from MySQL. records:', len(df_mysql)
+mysql_cn.close()
